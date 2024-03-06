ba52fc2d16ef4e6a7ee0fc94df2dd5d00b02dc64
VitorHSilver <vitor-henrique1996@hotmail.com>
2024-03-06 13:44:19 -0300

    Tipos de dados

---
+++ tiposDados/js.js
@@ -0,0 +1,8 @@
+var nome = 'Vitor ';
+var number = '08';
+var idade = 2024 - 1996,
+          sobreNome = 'Da Silva';
+var nomeCompleto = `${nome} ${sobreNome}`; // foi utilizado `` template literal para juntas para tornar a mais flexivel  
+console.log(nome, number, ';', idade);
+console.log(nomeCompleto);
+console.log(typeof nome);
