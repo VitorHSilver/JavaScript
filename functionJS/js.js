31ce2b6b2336e67c13593efb1b7e49374877be07
VitorHSilver <vitor-henrique1996@hotmail.com>
2024-05-12 14:22:01 -0300

    funcao botao

--- functionJS/js.js
+++ functionJS/js.js
@@ -1,11 +1,24 @@
 function Dom(seletor) {
-  this.element = document.querySelector(seletor);
-};
+     this.element = document.querySelector(seletor);
+}
 
-Dom.prototype.ativo = function(classe) {
-  this.element.classList.add(classe);
+Dom.prototype.ativo = function (classe) {
+     this.element.classList.add(classe);
 };
 
 const lista = new Dom('ul');
 lista.ativo('ativar');
 console.log(lista);
+
+// Criar uma função construtora de Button
+// que deverá conter
+// - class e text como parâmetros
+
+function Button(text, classe) {
+     this.element = document.createElement('button');
+     this.element.innerText = text;
+     this.element.classList.add(classe);
+     this.element.addEventListener('click', function () {
+          alert(text);
+     });
+}
